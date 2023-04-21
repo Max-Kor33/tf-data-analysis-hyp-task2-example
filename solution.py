3c4f15b1b737a5bef318606909725895e517e173
Max-Kor33 <62508152+Max-Kor33@users.noreply.github.com>
2023-04-21 20:29:56 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,14 +1,15 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import ks_2samp
+from hippo.ksample import MMD
 
 
 chat_id = 957195795 # Ваш chat ID, не меняйте название переменной
 
-def solution(x: np.array, y: np.array) -> bool:
-    stat, p_value = ks_2samp(x, y)
-    if p_value < 0.05:
-        res = False # отвергаем H0
+def solution(x, y):
+    ## Используем MMD
+    res = MMD(compute_kernel='rbf', gamma=1.0).test(x, y)
+    if res[1] < 0.03:
+        bool_final = True
     else:
-        res = True
-    return res # Ваш ответ, True или False
+        bool_final = False
+    return bool_final
